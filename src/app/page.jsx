fd622e54da1f5fcedfb8431a3b3e76ea9e9b5be3
nam <cungaphong11@gmail.com>
2023-05-12 23:53:17 +0700

    add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import HomeHeader from "@/components/HomeHeader";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <HomeHeader />
+    </>
   );
 }
